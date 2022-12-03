b8aefc402e2697d273bd3963b753ceccd1de4847
jaspreet kaur <kaurjas1928@yahoo.com>
2022-12-03 12:59:51 +0000

    fix the error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 import countriesAll from "./countriesAll.json";
 
 function App() {
   return (
-    <div className="App">
-{countriesAll.map((country)=>(
-  <div className="">
-    <p>{country.name.common}</p>
-    </div>
-))}
-    </div>
+   <ul>
+    {countriesAll.map((country, key) => {
+      return <p key={key}>{country.name}</p>
+    })}
+   </ul>
   );
 }
 
 export default App;
+/* <div className="App">
+      {countriesAll.map((country, key) => {
+        return (<div className="" key={key}>
+          <p>{country.name.common}</p>
+        </div>);
+})}
+    </div>
+    */
